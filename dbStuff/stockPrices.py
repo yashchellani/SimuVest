ed358aca517fbac6a6b1e28b8f007e8ed85e00f0
Yash Chellani <35882565+yashchellani@users.noreply.github.com>
2020-09-07 16:26:55 +0530

    Update stockPrices.py
    added real time updates

--- dbStuff/stockPrices.py
+++ dbStuff/stockPrices.py
@@ -3,8 +3,8 @@ from datetime import date
 from datetime import timedelta
 import pymongo
 from pymongo import MongoClient
-import stockanalysis
-stockSymbols= stockanalysis.getStockNames()
+import stockSymbolExtractor
+stockSymbols= stockSymbolExtractor.getStockNames()
 d1 = date.today()
 yesterday = d1 - timedelta(days = 1)
 cluster = MongoClient("mongodb+srv://yashc:Password1@cluster0.37ilo.mongodb.net/stockData?retryWrites=true&w=majority")
@@ -17,21 +17,52 @@ resultDic = sample_data.to_dict('list')
 #dictionary to be uploaded
 uploadDict = {}
 #uploadDict['date'] = date.today()
-collection.insert_one({'_id' : 'date', 'date' : d1.strftime("%d/%m/%Y")})
-for key in resultDic:
-    valueNum = resultDic[('Close', key[1])][0]
-    if valueNum == 'nan' or valueNum == 'NaN' or valueNum == None:
-        pass
-    uploadDict[key[1]] = {}
-    uploadDict[key[1]]['_id'] = key[1]
-    uploadDict[key[1]]['stockPrice'] = valueNum
+def findDate(todayDate = d1.strftime("%d/%m/%Y")):
+    #newQuery = {'date': todayDate}
+    #print (collection.find(newQuery))
+    #if collection.find(newQuery):
+     #   pass
+    #else:
+    try: 
+        collection.update_one({'_id' : 'date'},{"$set":{'date' : d1.strftime("%d/%m/%Y")}})
+        for key in resultDic:
+            valueNum = resultDic[('Close', key[1])][0]
+            if valueNum == 'nan' or valueNum == 'NaN' or valueNum == None:
+                pass
+            uploadDict[key[1]] = {}
+            uploadDict[key[1]]['_id'] = key[1]
+            uploadDict[key[1]]['stockPrice'] = valueNum
 
-#implement a check for date
-#inserting everything in the database
-for key in uploadDict:
-    if (type(uploadDict[key])) == 'datetime.date':
-        pass
-    else:
-        collection.insert_one(uploadDict[key])
+        for key in uploadDict:
+            if (type(uploadDict[key])) == 'datetime.date':
+                pass
+            else:
+                collection.update_one({'_id': key}, {"$set":{'stockPrice': uploadDict[key[1]]['stockPrice']}})
+    except:
+        print("There was an error in inserting your data.")
+
+findDate()
+def uploadData():
+    try: 
+        collection.insert_one({'_id' : 'date', 'date' : d1.strftime("%d/%m/%Y")})
+        for key in resultDic:
+            valueNum = resultDic[('Close', key[1])][0]
+            if valueNum == 'nan' or valueNum == 'NaN' or valueNum == None:
+                pass
+            uploadDict[key[1]] = {}
+            uploadDict[key[1]]['_id'] = key[1]
+            uploadDict[key[1]]['stockPrice'] = valueNum
+
+        #implement a check for date
+        #inserting everything in the database
+        for key in uploadDict:
+            if (type(uploadDict[key])) == 'datetime.date':
+                pass
+            else:
+                collection.insert_one(uploadDict[key])
+    except: 
+        print("There was an error in inserting your data.")
+    finally:
+        print("Process complete.")
 
 
